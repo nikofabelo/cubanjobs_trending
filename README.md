bbf7a476eff8f39ce7f771f0c1de82e87372ecc0
Yoel N. Fabelo González <human.x7e6@gmail.com>
2024-08-03 01:27:49 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # cubanjobs_trending
+
+Streamlit app showcasing demand over time of some skills at <a href="https://t.me/cubanjobs">cubanjobs</a>.
+
+## How to use?
+Go <a href="https://my.telegram.org"/>, get an `API_ID` and `API_HASH` and write them to `secrets.json`.
+
+Install requirements with:<br>
+- `pip install -r requirements.txt`
+Then run the app with:<br>
+- `streamlit run app.py`
+
+## Screenshots
+![Screenshot_1.png](Screenshot_1.png)
+![Screenshot_2.png](Screenshot_2.png)
---
+++ app.py
@@ -0,0 +1,147 @@
+import json, sys, asyncio
+from telethon import TelegramClient
+from telethon.tl.functions.messages import GetHistoryRequest
+import streamlit as st
+import plotly.graph_objects as go
+
+with open('secrets.json') as rf:
+	values = json.load(rf)
+	api_id = values['API_ID']
+	api_hash = values['API_HASH']
+
+if len(sys.argv) > 1:
+	limit = int(sys.argv[1])
+else:
+	limit = 100
+
+async def fetch_messages():
+	async with TelegramClient('telethon', api_id=api_id, api_hash=api_hash) as client:
+		await client.start()
+
+		group = await client.get_entity('cubanjobs')
+
+		history = await client(
+			GetHistoryRequest(
+				peer=group, limit=limit, offset_date=None, offset_id=0,
+				max_id=0, min_id=0, add_offset=0, hash=0
+			)
+		)
+
+		return history.messages
+
+result = asyncio.run(fetch_messages())
+
+SKILLS = {
+	'angular': 'Angular',
+	'aspnet': 'ASP.NET',
+	'c#': 'C#',
+	'django': 'Django',
+	'flutter': 'Flutter',
+	'javascript': 'JavaScript',
+	'laravel': 'Laravel',
+	'nodejs': 'Node.js',
+	'php': 'PHP',
+	'python': 'Python',
+	'react': 'React',
+	'typescript': 'TypeScript',
+	'vuejs': 'Vue.js'
+}
+
+messages = []
+
+for message in result:
+	if not message.message is None and message.message.strip():
+		message = message.message
+
+		message = ''.join(
+			c for c in message.lower() if c.isalpha() or c.isspace() or c in '/+#' # (+) not split, for C++
+		).replace('/', ' ').split()
+
+		for skill in list(SKILLS.keys()):
+			if skill in message:
+				if not message in messages:
+					messages.append(message)
+					break
+
+data = {}
+
+for skill in list(SKILLS.keys()):
+	data[skill] = [0] * len(messages)
+
+for i in range(len(messages)):
+	for word in messages[i]:
+		if word in list(SKILLS.keys()):
+			data[word][i] = 1
+
+for k in list(data.keys()):
+	if sum(data[k]) == 0:
+		del data[k]
+
+data = {SKILLS[k]:	v for k, v in data.items()}
+
+st.set_page_config(
+	layout='wide',
+	page_icon='📈',
+	page_title='cubanjobs Trending'
+)
+
+st.title('[cubanjobs](https://t.me/cubanjobs) Trending')
+
+skills_demand = {k:	sum(v) for k, v in data.items()}
+
+max_index = list(skills_demand.values()).index(max(list(skills_demand.values())))
+
+pie_chart = go.Figure(
+	data=[
+		go.Pie(
+			hole=0.4,
+			labels=list(skills_demand.keys()),
+			pull=[0.1 if i == max_index else 0 for i in range(len(skills_demand))],
+			textinfo='percent+label',
+			textposition='outside',
+			values=list(skills_demand.values())
+		)
+	]
+)
+
+pie_chart.update_layout(title='Tech Skills Demand')
+
+with st.expander('Market Data'):
+	col1, col2 = st.columns(2, gap='large')
+
+	with col1:
+		st.subheader('Data Table')
+		st.dataframe(
+			[
+				{'Skill':	k, 'Demand':	v}
+				for k, v in skills_demand.items()
+			],
+			use_container_width=True
+		)
+
+	with col2:
+		st.subheader('Pie Chart')
+		st.plotly_chart(pie_chart, use_container_width=True)
+
+line_plot = go.Figure()
+
+for skill, v in data.items():
+	line_plot.add_trace(
+		go.Scatter(
+			fill='tozeroy',
+			mode='lines',
+			name=skill,
+			x=list(range(1, len(v) + 1, 1)),
+			y=[i * skills_demand[skill] for i in v]
+		)
+	)
+
+line_plot.update_layout(
+	height=500,
+	title='Skill Demand by Message',
+	xaxis_title='Message Count',
+	yaxis_title='Skill Demand Count'
+)
+
+st.subheader('Line Plot')
+st.plotly_chart(line_plot, use_container_width=True)
